e9cb934ab85267b0d2a203e9b65f80274c2425ad
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-10-29 18:02:55 +0200

    Create TurtleSquare.py

---
+++ TurtleSquare.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.forward(125)
+turtle.left(90)
+turtle.forward(125)
+turtle.left(90)
+turtle.forward(125)
+turtle.left(90)
+turtle.forward(125)
+turtle.left(90)
+
+turtle.done()
