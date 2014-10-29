3e99a19f2eb575004835ebae1b4b6f0fb9bb1326
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-10-29 18:05:05 +0200

    Create TiltedSquare.py

---
+++ TiltedSquare.py
@@ -0,0 +1,42 @@
+import turtle
+# Tilted square
+# -------------
+turtle.shape("turtle")
+turtle.pensize(4)
+turtle.bgcolor("yellow")
+
+turtle.color("blue", "red")
+turtle.begin_fill()
+
+turtle.left(20)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+
+turtle.left(20)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+
+turtle.left(20)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+
+turtle.end_fill()
+turtle.done()
