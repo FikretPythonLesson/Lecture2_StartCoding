d807a15d84de54e9645d082bbb84ba76db3b603d
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-10-29 18:04:35 +0200

    Create Triangle.py

---
+++ Triangle.py
@@ -0,0 +1,19 @@
+import turtle
+
+# Triangle
+# --------
+turtle.shape("turtle")
+turtle.pensize(4)
+turtle.bgcolor("yellow")
+turtle.color("blue", "red")
+turtle.begin_fill()
+
+turtle.forward(150)
+turtle.left(120)
+turtle.forward(150)
+turtle.left(120)
+turtle.forward(150)
+turtle.left(120)
+
+turtle.end_fill()
+turtle.done()
