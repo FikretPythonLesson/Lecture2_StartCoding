14be16eb7df82fab4cd6b00a2e788d3ce800bb49
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-10-29 18:19:00 +0200

    Create shapesAll.py
    Draw square, trianle, hexagon and octagon

---
+++ shapesAll.py
@@ -0,0 +1,19 @@
+import turtle
+
+for _ in range(3):
+    turtle.forward(100)
+    turtle.left(120)
+    
+for _ in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+for _ in range(6):
+    turtle.forward(100)
+    turtle.left(60)
+
+for _ in range(8):
+    turtle.forward(100)
+    turtle.left(360/8)
+    
+turtle.done()    
