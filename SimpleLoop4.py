c05c2b37b1fcce03749f7be3a0799e7e852ec0d7
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-10-29 18:08:20 +0200

    Create SimpleLoop4.py

---
+++ SimpleLoop4.py
@@ -0,0 +1,8 @@
+# -*- coding: cp1254 -*-
+
+Mk1Students = ["Ahmet", "Kaan", "Ömer", "İlke", "Onur", "Ege", "Hüseyin", "Cüneyt", "İlke"]
+for student in Mk1Students:
+    print student, " loves python"
+
+
+print "... and other Mk1 students also loves python"
