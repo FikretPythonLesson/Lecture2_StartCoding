24902a030fa93f1d01a79533df6a0d48328d9915
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-10-29 18:04:03 +0200

    Create SquareWithColor.py

---
+++ SquareWithColor.py
@@ -0,0 +1,23 @@
+import turtle
+
+# Square 
+# ------
+turtle.shape("turtle")
+turtle.pensize(4)
+turtle.bgcolor("yellow")
+
+turtle.color("blue", "red")
+turtle.begin_fill()
+
+turtle.forward(125)
+turtle.left(90)
+turtle.forward(125)
+turtle.left(90)
+turtle.forward(125)
+turtle.left(90)
+turtle.forward(125)
+turtle.left(90)
+
+turtle.end_fill()
+
+turtle.done()
